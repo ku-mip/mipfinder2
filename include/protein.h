f5f856598bd79f8c9ac77d8b21942ee8b5f795c3
ku-mip <valdeko@plen.ku.dk>
2021-06-02 13:10:12 +0200

    refactor: Fixed formatting

--- include/protein.h
+++ include/protein.h
@@ -12,84 +12,89 @@
 #include "go.h"
 #include "interpro.h"
 
-namespace mipfinder {
+namespace mipfinder
+{
 
-  //Protein holds information about one specific protein isoform. The Protein identifier is composed
-  //of UniProt accession name plus the sequence version.
-  class Protein {
-  public:
-    enum class Type {
-      UNKNOWN,
-      CMIP,
-      SINGLE_COPY,
-      HOMOLOGOUS,
-      ANCESTOR,
-      KNOWN_MIP
-    };
+	//Protein holds information about one specific protein isoform. The Protein identifier is composed
+	//of UniProt accession name plus the sequence version.
+	class Protein
+	{
+	public:
+		enum class Type
+		{
+			UNKNOWN,
+			CMIP,
+			SINGLE_COPY,
+			HOMOLOGOUS,
+			ANCESTOR,
+			KNOWN_MIP
+		};
 
-    Protein() = delete;
-    Protein(const std::string& identifier,
-            const std::string& sequence,
-            const std::string& description,
-            int protein_existence);
-    ~Protein() = default;
+		Protein() = delete;
+		Protein(const std::string& identifier,
+				const std::string& sequence,
+				const std::string& description,
+				int protein_existence);
+		~Protein() = default;
 
-    std::string identifier() const;
-    std::string sequence() const;
-    std::string description() const;
+		std::string identifier() const;
+		std::string sequence() const;
+		std::string description() const;
 
-    std::vector<mipfinder::Ancestor> ancestors() const;
-    void addAncestor(const Ancestor& ancestor);
+		std::vector<mipfinder::Ancestor> ancestors() const;
+		void addAncestor(const Ancestor& ancestor);
 
-    std::vector<mipfinder::Homologue> homologues() const;
-    void addHomologue(const Homologue& homologue);
+		std::vector<mipfinder::Homologue> homologues() const;
+		void addHomologue(const Homologue& homologue);
 
-    Type type() const;
-    std::string type_to_string() const;
-    void setType(const mipfinder::Protein::Type& type);
+		Type type() const;
+		std::string type_to_string() const;
+		void setType(const mipfinder::Protein::Type& type);
 
-    mipfinder::Go::Entries goEntries() const;
-    void addGoEntry(const mipfinder::Go::Entry& entry);
+		mipfinder::Go::Entries goEntries() const;
+		void addGoEntry(const mipfinder::Go::Entry& entry);
 
-    mipfinder::Interpro::Entries interproEntries() const;
-    void addInterproEntry(const mipfinder::Interpro::Entry& entry);
+		mipfinder::Interpro::Entries interproEntries() const;
+		void addInterproEntry(const mipfinder::Interpro::Entry& entry);
 
-    int existenceLevel() const;
+		int existenceLevel() const;
 
-    std::size_t length() const;
-    double score() const;
-    void changeScore(double score);
-  private:
-    std::string identifier_;
-    std::string sequence_;
-    std::string description_;
-    int existence_level_;
-    double score_;
+		std::size_t length() const;
+		double score() const;
+		void changeScore(double score);
+	private:
 
-    mipfinder::Protein::Type type_;
-    std::vector<mipfinder::Ancestor> ancestors_;
-    std::vector<mipfinder::Homologue> homologues_;
 
-    mipfinder::Interpro::Entries interpro_entries_;
-    mipfinder::Go::Entries go_entries_;
-  };
+		std::string identifier_;
+		std::string sequence_;
+		std::string description_;
+		int existence_level_;
+		double score_;
 
-  /* Returns the instability index of the sequence */
-  double instability_index(const std::string& sequence);
+		mipfinder::Protein::Type type_;
+		std::vector<mipfinder::Ancestor> ancestors_;
+		std::vector<mipfinder::Homologue> homologues_;
 
-  bool operator==(const Protein& lhs, const Protein& rhs);
+		mipfinder::Interpro::Entries interpro_entries_;
+		mipfinder::Go::Entries go_entries_;
+	};
 
-  //Creates a FASTA file from existing Proteins
-  template <typename Container>
-  void proteinToFasta(const Container& proteins, 
-                      const std::filesystem::path& output)
-  {
-    std::ofstream f;
-    f.open(output);
-    for (const auto& protein : proteins) {
-      f << ">" << protein->identifier() << "\n" << protein->sequence() << "\n";
-    }
-  }
+	/* Returns the instability index of the sequence */
+	double instability_index(const std::string& sequence);
+
+	bool operator==(const Protein& lhs, const Protein& rhs);
+
+	//Creates a FASTA file from existing Proteins
+	template <typename Container>
+	void proteinToFasta(const Container& proteins,
+						const std::filesystem::path& output)
+	{
+		std::ofstream f;
+		f.open(output);
+		for (const auto& protein : proteins) {
+			f << ">" << protein->identifier() << "\n" << protein->sequence() << "\n";
+		}
+	}
 
 }
 #endif

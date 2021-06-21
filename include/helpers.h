dadf52478f0ba5d25ad200c65049899a13cbc49c
Valdeko Kruusvee <kruusvee.valdeko@gmail.com>
2021-06-21 16:58:19 +0200

    refactor: toContainer works...kind of
    The toContainer function from a range is very basic but it'll do until C++23 comes out

--- include/helpers.h
+++ include/helpers.h
@@ -20,16 +20,12 @@ namespace detail
 {
     //TODO: Only really works for containers that have push_back operation but it is fine for
     //this application. Best to wait for std::ranges::to in C++23.
-    template <typename T, typename U>
-    requires std::ranges::range<T>
-    auto toContainer(T&& range)
+    template <template <typename> typename Container, typename Range>
+    requires std::ranges::range<Range>
+        Container<std::ranges::range_value_t<Range>> toContainer(Range&& range)
     {
-        using View = decltype(std::declval<T>().base());
-        using ContainerType = std::remove_cv_t<std::remove_reference_t<decltype(std::declval<View>().base())>>;
-        ContainerType container;
-        // auto container_size = std::distance(std::begin(range_view), std::end(range_view));
-        // container.resize(container_size);
-        std::ranges::copy(range_view, std::back_inserter(container));
+        Container<std::ranges::range_value_t<Range>> container;
+        std::ranges::copy(range, std::back_inserter(container));
         return container;
     }
   

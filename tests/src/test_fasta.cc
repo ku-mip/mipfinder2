388b7dc13aa4816b59eb3e1ec74e5122c90ef7d2
Valdeko Kruusvee <kruusvee.valdeko@gmail.com>
2021-08-05 10:31:03 +0200

    tests: Add more tests to fasta

--- tests/src/test_fasta.cc
+++ tests/src/test_fasta.cc
@@ -7,16 +7,30 @@ TEST(TestExtractRecords, ParseValidFile_FindAllRecords)
     EXPECT_EQ(extracted_records.size(), 5);
 }
 
-TEST(TestExtractRecords, ParseFileWithBadRecords_FindAllValidRecords)
+TEST(TestExtractRecords, ParseFileWithGoodAndBadRecords_FindAllValidRecords)
 {
     auto extracted_records = mipfinder::fasta::extractRecords("mixed.fasta");
     EXPECT_EQ(extracted_records.size(), 6);
 }
 
+TEST(TestExtractRecords, ParseFileWithNoRecords_FindNoValidRecords)
+{
+    auto extracted_records = mipfinder::fasta::extractRecords("empty.fasta");
+    EXPECT_EQ(extracted_records.size(), 0);
+}
 
+TEST(TestExtractRecords, ParseRecordWithInvalidHeader_FindNoValidRecords)
+{
+    auto extracted_records = mipfinder::fasta::extractRecords("empty_header.fasta");
+    EXPECT_EQ(extracted_records.size(), 0);
+}
+
+TEST(TestExtractRecords, ParseNonExistentFile_ThrowStdRuntimeError)
+{
+    EXPECT_THROW(mipfinder::fasta::extractRecords("does_not_exist.fasta"), std::runtime_error);
+}
 
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     RUN_ALL_TESTS();
-    std::cin.get();
 }

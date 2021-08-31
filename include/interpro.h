f7e0c815b4bc1cdb0f734f8faf3ce1ae32368a58
Valdeko Kruusvee <kruusvee.valdeko@gmail.com>
2021-08-31 14:13:06 +0200

    refactor: Complete rework of Interpro class

--- include/interpro.h
+++ include/interpro.h
@@ -3,58 +3,91 @@
 
 #include <filesystem>
 #include <string>
-#include <unordered_map>
-#include <unordered_set>
- 
-namespace mipfinder::interpro
+#include <vector>
+
+namespace mipfinder
 {
-    //Entry class denotes a unique InterPro entry. Every possible InterPro annotation for a protein
-    //is composed from InterPro entries. 
-    struct Entry
-    {
-        enum class Type {
-            active_site,
-            binding_site,
-            conserved_site,
-            domain_type,
-            family,
-            homologous_superfamily,
-            ptm,
-            repeat,
-            unknown
+    /**
+     *  @class  Interpro interpro.h "include/interpro.h"
+     *  @brief  Provide a collection of InterPro entries.
+     *
+     *  @invariant  1. The collection is always sorted according to the InterPro
+     *              entry accession value in a lexicographical order.
+     *              2. The entry accessions are unique.
+     */
+    class Interpro {
+    public:
+        /**
+         *  @class  Entry interpro.h "include/interpro.h"
+         *  @brief  Represents a single InterPro entry.
+         *
+         *  Every InterPro entry has an accession identifier, a name (also
+         *  known as description), and an entry type.
+         */
+        struct Entry {
+            enum class Type {
+                active_site,
+                binding_site,
+                conserved_site,
+                domain_type,
+                family,
+                homologous_superfamily,
+                ptm,
+                repeat,
+                unknown
+            };
+            std::string name;
+            std::string accession;
+            Type type;
+            auto operator<=>(const Entry&) const = default;
         };
-        std::string name;
-        std::string accession;
-        Type type;
-        auto operator<=>(const Entry&) const = default;
+
+        /**
+         *  @brief  Initialise a collection of InterPro entries.
+         *  @param entries  A three-column tsv-file that details each InterPro
+         *                  entry (available from
+                            https://ftp.ebi.ac.uk/pub/databases/interpro/entry.list).
+         *                  Column 1: InterPro entry accession.
+         *                  Column 2: InterPro entry type.
+         *                  Column 3: InterPro entry name (description).
+         *  @throw std::runtime_error If @a entries cannot be opened.
+         */
+        Interpro(const std::filesystem::path& entries);
+
+        using const_iterator = std::vector<Entry>::const_iterator;
+        const_iterator begin() const;
+        const_iterator cbegin() const;
+        const_iterator end() const;
+        const_iterator cend() const;
+
+    private:
+        std::vector<Entry> m_entries;
     };
 
-    using Entries = std::vector<Entry>;
-    /* @interpro_entry_list is a tsv-file that details each InterPro entry (available from https://ftp.ebi.ac.uk/pub/databases/interpro/entry.list [accessed 06/08/21]).
-     * Column 1: InterPro identifier
-     * Column 2: Identifier type
-     * Column 3: Identifier description
-     * 
-     * Exceptions
-     *  If `interpro_entry_list` cannot be opened, throws std::runtime_error
-     * 
-     * Return a list of InterPro entries in indeterminate order.
+    /**
+     * @brief  Find InterPro entry by its accession.
+     * @param interpro_database  Database to search.
+     * @param entry_accession  Accession of the entry to search for.
+     * @return Read-only (constant) iterator to the given InterPro entry, or
+     *         iterator equivalent to Interpro::cend() if the entry could not be
+     *         be found.
      */
-    Entries parseEntryList(const std::filesystem::path& interpro_entry_list);
+    Interpro::const_iterator find(const Interpro& interpro_database, const std::string& entry_accession);
 
-    using ProteinDomains = std::unordered_map<std::string, std::unordered_set<std::string>>;
 
-    //Map every UniProt ID to all the InterPro domains the protein contains.
-    //
-    //@uniprot_to_interpro_table is a tsv-file that specifies which UniProt entry has which InterPro
-    //domains
-    //Column 1: UniProt ID (without sequence version)
-    //Column 2: Sequence version of that corresponding UniProt ID
-    //Column 3: A comma (;) separated list of InterPro entry identifiers
-    //
-    //Return an associative array where the keys are UniProt identifiers (including the sequence version) and the
-    //values are a set of InterPro identifiers corresponding to the InterPro entires that protein has been
-    //annotated to have. If the input file is not in a correct format, the behaviour is unspecified.
-    ProteinDomains parseProteinDomainList(const std::filesystem::path& uniprot_to_interpro_table);
+    //using ProteinDomains = std::unordered_map<std::string, std::unordered_set<std::string>>;
+
+    ////Map every UniProt ID to all the InterPro domains the protein contains.
+    ////
+    ////@uniprot_to_interpro_table is a tsv-file that specifies which UniProt entry has which InterPro
+    ////domains
+    ////Column 1: UniProt ID (without sequence version)
+    ////Column 2: Sequence version of that corresponding UniProt ID
+    ////Column 3: A comma (;) separated list of InterPro entry identifiers
+    ////
+    ////Return an associative array where the keys are UniProt identifiers (including the sequence version) and the
+    ////values are a set of InterPro identifiers corresponding to the InterPro entires that protein has been
+    ////annotated to have. If the input file is not in a correct format, the behaviour is unspecified.
+    //ProteinDomains parseProteinDomainList(const std::filesystem::path& uniprot_to_interpro_table);
 }
 #endif
--- src/interpro.cc
+++ src/interpro.cc
@@ -2,96 +2,126 @@
 #include "interpro.h"
 #include "protein.h"
 
-namespace mipfinder::interpro
-{
+namespace detail {
 
-    //Parse the Interpro entry list (available from https://ftp.ebi.ac.uk/pub/databases/interpro/entry.list [accessed 06/08/21]).
-    //The format of the supplied interpro_entry_list should be a tab-separated file consisting of interpro accession number,
-    //entry type and entry name. E.g. "IPR000126	Active_site	Serine proteases, V8 family, serine active site"
-    //The first line of the file should be the three headers for the columns. If the file
-    //does not correspond to the given, the behaviour is undefined.
-    //Returns a list of InterPro entries in a non-determinate order.
-    Entries parseEntryList(const std::filesystem::path& interpro_entry_list)
+    /* Return a list of individual InterPro entries.
+     *
+     * @interpro_entry_list is a tsv-file that details each InterPro entry (available from https://ftp.ebi.ac.uk/pub/databases/interpro/entry.list [accessed 06/08/21]).
+     * Column 1: InterPro identifier
+     * Column 2: Identifier type
+     * Column 3: Identifier description
+     *
+     * The first line of the file must contain the headers for the columns. If the
+     * file does not correspond to the given format, the behaviour is undefined.
+     *
+     * Exceptions
+     *  If `interpro_entry_list` cannot be opened, throws std::runtime_error
+     *
+     * Return a list of InterPro entries in indeterminate order.
+     */
+    std::vector<mipfinder::Interpro::Entry> parseInterproEntryList(const std::filesystem::path& interpro_entry_list)
     {
         std::ifstream file{ interpro_entry_list };
         if (!file.is_open()) {
-            throw std::runtime_error("Cannot open " + interpro_entry_list.string() + ", aborting...");
+            throw std::runtime_error("Cannot open " + interpro_entry_list.string() +
+                ", aborting...");
         }
 
-        mipfinder::interpro::Entries results;
+        std::vector<mipfinder::Interpro::Entry> results;
         std::string line;
-        std::getline(file, line); /* Skips the header in database file */
+        std::getline(file, line); //Skip the header in database file
         while (std::getline(file, line)) {
             const auto tokens = mipfinder::tokenise(line, '\t');
 
-            /* Correctly formatted file has three columns of data */
+            //Correctly formatted file has three columns of data
             if (tokens.size() != 3) {
                 continue;
             }
 
-            mipfinder::interpro::Entry::Type type;
+            mipfinder::Interpro::Entry::Type type;
             const auto& domain_type = tokens[1];
             if (domain_type == "Active_site") {
-                type = mipfinder::interpro::Entry::Type::active_site;
+                type = mipfinder::Interpro::Entry::Type::active_site;
             }
             else if (domain_type == "Binding_site") {
-                type = mipfinder::interpro::Entry::Type::binding_site;
+                type = mipfinder::Interpro::Entry::Type::binding_site;
             }
             else if (domain_type == "Conserved_site") {
-                type = mipfinder::interpro::Entry::Type::conserved_site;
+                type = mipfinder::Interpro::Entry::Type::conserved_site;
             }
             else if (domain_type == "Domain") {
-                type = mipfinder::interpro::Entry::Type::domain_type;
+                type = mipfinder::Interpro::Entry::Type::domain_type;
             }
             else if (domain_type == "Family") {
-                type = mipfinder::interpro::Entry::Type::family;
+                type = mipfinder::Interpro::Entry::Type::family;
             }
             else if (domain_type == "Homologous_superfamily") {
-                type = mipfinder::interpro::Entry::Type::homologous_superfamily;
+                type = mipfinder::Interpro::Entry::Type::homologous_superfamily;
             }
             else if (domain_type == "PTM") {
-                type = mipfinder::interpro::Entry::Type::ptm;
+                type = mipfinder::Interpro::Entry::Type::ptm;
             }
             else if (domain_type == "Repeat") {
-                type = mipfinder::interpro::Entry::Type::repeat;
+                type = mipfinder::Interpro::Entry::Type::repeat;
             }
             else {
-                type = mipfinder::interpro::Entry::Type::unknown;
+                type = mipfinder::Interpro::Entry::Type::unknown;
             }
 
             const auto& interpro_accession = tokens[0];
             const auto& entry_name = tokens[2];
-            results.emplace_back(mipfinder::interpro::Entry{.name = entry_name, .accession = interpro_accession, .type = type});
+            results.emplace_back(mipfinder::Interpro::Entry{
+                .name = entry_name, .accession = interpro_accession, .type = type });
         }
         return results;
     }
+} // namespace detail
 
-    ProteinDomains parseProteinDomainList(const std::filesystem::path& uniprot_to_interpro_table)
+namespace mipfinder
+{
+    Interpro::Interpro(const std::filesystem::path& interpro_entry_list)
+        : m_entries(detail::parseInterproEntryList(interpro_entry_list))
     {
-        std::ifstream file{ uniprot_to_interpro_table };
-        if (!file.is_open()) {
-            throw std::runtime_error("Cannot open " + uniprot_to_interpro_table.string() + ", aborting...");
-        }
-
-        std::string line;
-        mipfinder::interpro::ProteinDomains results;
-        while (std::getline(file, line)) {
-            const auto tokens = mipfinder::tokenise(line, '\t');
-
-            /* Correctly formatted file has three columns of data */
-            if (tokens.size() != 3) {
-                continue;
-            }
-
-            std::string uniprot_id = tokens[0];
-            std::string sequence_version = tokens[1];
-            std::string full_protein_id = uniprot_id + sequence_version;
-            constexpr auto domain_entry_delimiter = mipfinder::Protein::id_delimiter;
-            std::vector<std::string> interpro_identifiers = mipfinder::tokenise(tokens[2], domain_entry_delimiter);
-            for (const auto& identifier : interpro_identifiers) {
-                results[full_protein_id].insert(identifier);
-            }
-        }
-        return results;
+        std::sort(std::begin(m_entries), std::end(m_entries));
     }
-}
+
+    Interpro::const_iterator Interpro::begin() const
+    {
+        return m_entries.begin();
+    }
+
+    Interpro::const_iterator Interpro::cbegin() const
+    {
+        return m_entries.cbegin();
+    }
+
+    Interpro::const_iterator Interpro::end() const
+    {
+        return m_entries.end();
+    }
+
+    Interpro::const_iterator Interpro::cend() const
+    {
+        return m_entries.cend();
+    }
+
+    Interpro::const_iterator find(const Interpro& interpro_database, const std::string& entry_accession)
+    {
+        auto comparator = [](const Interpro::Entry& element,
+            const std::string& accession_to_find) {
+                return element.accession < accession_to_find;
+        };
+
+        auto returned_entry = std::lower_bound(
+            std::cbegin(interpro_database), std::cend(interpro_database), entry_accession, comparator);
+
+        if (returned_entry != std::cend(interpro_database) &&
+            (*returned_entry).name == entry_accession) {
+            return returned_entry;
+        }
+        else {
+            return interpro_database.cend();
+        }
+    }
+
+} // namespace mipfinder

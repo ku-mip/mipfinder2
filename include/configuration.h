47c900964916f1e9cf0b70b097047ee68a77a26f
Valdeko Kruusvee <kruusvee.valdeko@gmail.com>
2022-03-21 14:11:15 +0100

    Savepoint before removing templates

--- include/configuration.h
+++ include/configuration.h
@@ -5,26 +5,125 @@
 #include <string>
 #include <unordered_map>
 #include <vector>
+#include <string>
+
+#include "easylogging++.h"
+
+namespace mipfinder
+{
+    template <typename Parameter>
+    struct Configuration;
+
+    //Extracts the parameter=value from configuration file and saves them to the Configuration object.
+    //uses regex to ensure that any user-formatting errors do not affect the parsing.
+    template <typename Parameter>
+    Configuration<Parameter> parse(const std::filesystem::path& configuration_file)
+    {
+        LOG(DEBUG) << "Parsing configuration file (" + configuration_file.string() + ")";
+        std::ifstream f{ configuration_file };
+        if (!f.is_open()) {
+            throw std::runtime_error("Could not open the configuration file \"" + configuration_file.string() + "\" for parsing");
+        }
+
+        std::string line;
+        //unsigned int config_line_nr = 0;
+        Configuration configuration;
+        while (getline(f, line)) {
+            //    ++config_line_nr;
+
+            static constexpr auto comment_line_token = '#';
+            if (line.empty()) { continue; }
+            if (line.front() == comment_line_token) { continue; }
+
+            static const std::regex header_regex(R"((?:\[)(\w+)(?:\]$))");
+            static const std::regex parameter_regex(R"((\w+)(?:\W*=\W*)(\w+))");
+
+            Configuration<Parameter>::Header current_header;
+            if (std::std::match_results<Parameter::name_type> matches; std::regex_search(line, matches, header_regex)) {
+                current_header = matches[1];
+                LOG(DEBUG) << "A valid header with value \"" + header << +"\" was found";
+                continue;
+            }
+
+            if (std::smatch matches; std::regex_search(line, matches, parameter_regex)) {
+                std::string parameter = matches[1];
+                std::string value = matches[2];
+
+                if (value.empty() && !is_optional) {
+                    //If parameter is not set as optional, value cannot be empty! Leaving it empty is a logic
+                    //error.
+                    throw std::logic_error(configuration_file.string() + ":" + std::to_string(config_line_nr) + ": Parameter ("
+                        + parameter + ") is not marked as optional and must have a value");
+                }
+
+                LOG(DEBUG) << "A valid parameter \"" + parameter + "\" with value \"" + value + "\" was found";
+                if (added_params.find(parameter) == added_params.end()) {
+                    config[header].insert(parameter, value);
+                    added_params.insert(parameter);
+                    LOG(DEBUG) << "Added " + parameter + " with " + value + " to " + header;
+                    continue;
+                }
+                else {
+                    throw std::logic_error(configuration_file.string() + ":" + std::to_string(config_line_nr) + ": Duplicate"
+                        " parameters (" + parameter + ") are not allowed");
+                }
+
+        }
+
+
+        //    //The logic is as follows:
+        //    //First check for the header line. Due to the way the regexes were written, the parameter=value 
+        //    //regex also matches the header line, therefore we have to check for that first.
+        //    //If there is no match, check whether the line contains the word ":optional:". This signals that
+        //    //the parameter is optional. The flag is set and the line is then matched against the
+        //    //parameter=value regex, extracting the information.
+        //    static const std::regex header_regex(R"((?:^\[)(\S+)(?:\]))");
+        //    static const std::regex param_value_regex(R"((?:^[\s]+)*(?:\:optional\:)?([\S]+)(?:[=])([\S]+)*)");
+        //    std::smatch matches;
+
+
+
+        //    bool is_optional = false;
+        //    if (line.find(":optional:") != std::string::npos) {
+        //        LOG(DEBUG) << "Optional token was found";
+        //        is_optional = true;
+        //    }
+
+        //    if (std::regex_search(line, matches, param_value_regex)) {
+        //        std::string parameter = matches[1];
+        //        std::string value = matches[2];
+
+        //        if (value.empty() && !is_optional) {
+        //            //If parameter is not set as optional, value cannot be empty! Leaving it empty is a logic
+        //            //error.
+        //            throw std::logic_error(configuration_file.string() + ":" + std::to_string(config_line_nr) + ": Parameter ("
+        //                + parameter + ") is not marked as optional and must have a value");
+        //        }
+
+        //        LOG(DEBUG) << "A valid parameter \"" + parameter + "\" with value \"" + value + "\" was found";
+        //        if (added_params.find(parameter) == added_params.end()) {
+        //            config[header].insert(parameter, value);
+        //            added_params.insert(parameter);
+        //            LOG(DEBUG) << "Added " + parameter + " with " + value + " to " + header;
+        //            continue;
+        //        }
+        //        else {
+        //            throw std::logic_error(configuration_file.string() + ":" + std::to_string(config_line_nr) + ": Duplicate"
+        //                " parameters (" + parameter + ") are not allowed");
+        //        }
+        //    }
+        //}
+        //LOG(DEBUG) << "Finised parsing configuration file";
+        //return config;
+    }
+}
+
 
 namespace mipfinder {
 
     /* Parameter class is is a wrapper around an std::unordered_map to overload
      * the operator[] */
-    class Parameters {
-    public:
-        using Parameter = std::string;
-        using Value = std::string;
 
-        Parameters() = default;
-        void insert(const Parameter& parameter, const Value& value);
-
-        Value& operator[](const Parameter& p);
-        const Value& operator[](const Parameter& p) const;
-
-        std::size_t size() const noexcept;
-    private:
-        std::unordered_map<Parameter, Value> params_;
-    };
 
     /* Configuration is a class to parse a configuration file and provide an uniform access to global
      * parameters set in a file. Every configuration object can have its own configuration file
@@ -42,29 +141,69 @@ namespace mipfinder {
      * file.
      * Any parameters NOT set as :optional: must have a value. If a compulsory paramater does not have
      * a value, the class throws std::invalid_argument */
-    class Configuration {
+    template <typename Name, typename Value>
+    struct Basic_Parameter
+    {
+        using name_type = Name;
+        using value_type = Value;
+        Name name;
+        Value value;
+    };
+
+    using Parameter = Basic_Parameter<std::string, std::string>;
+
+    template <typename Parameter = Parameter>
+    class Configuration
+    {
     public:
         using Header = std::string;
+        using ParameterName = Parameter::name_type;
+        using ParameterValue = Parameter::value_type;
 
         Configuration() = default;
-        Configuration(const std::filesystem::path& configuration_file);
+        Configuration(std::filesystem::path configuration_file)
+        {
+            parse<Parameter>(configuration_file);
+        }
 
-        /**
-         * @param  header  The header value to return parameters for.
-         *
-         * @return  Parameters for the given header. If the header does not exist, returns
-         *          empty Parameter.
-         */
-        Parameters& operator[](const Header& header);
-        const Parameters& operator[](const Header& header) const;
+        void insert(const Header& header, const Parameter& parameter)
+        {
+            parameters[header].push_back(parameter);
+        };
 
-        std::size_t size() const noexcept;
+        void insert(const Header& header)
+        {
+            parameters[header];
+        }
+
+        bool contains(const Header& header, const ParameterName& parameter_name)
+        {
+            for (const auto& param : parameters[header]) {
+                if (parameter_name == param.name) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        ParameterValue& value(const Header& header, const ParameterName& parameter_name)
+        {
+            for (const auto& param : parameters[header]) {
+                if (parameter_name == param.name) {
+                    return param.value;
+                }
+            }
+            throw std::out_of_range("No parameter under header \"" + header + "\" with name \"" + parameter_name + "\" found");
+        };
+
+        std::size_t size() const noexcept
+        {
+            return parameters.size();
+        }
+
     private:
-        //A configuration file is represented by a map of maps, where the keys of the outer map are the
-        //headers and the keys of the inner map are the parameters. The values of the inner map are the
-        //parameters values.
-        std::unordered_map<Header, Parameters> config_;
-        std::filesystem::path config_file_;
+        std::unordered_map<Header, std::vector<Parameter>> parameters;
+        std::filesystem::path configuration_file;
     };
 
 }
--- src/configuration.cc
+++ src/configuration.cc
@@ -8,124 +8,80 @@
 
 namespace
 {
-	//Extracts the parameter=value from configuration file and saves them to the Configuration object.
-	//uses regex to ensure that any user-formatting errors do not affect the parsing.
-	mipfinder::Configuration parse(const std::filesystem::path& configuration_file)
-	{
-		LOG(DEBUG) << "Parsing configuration file (" + configuration_file.string() + ")";
-		std::ifstream f;
-		f.open(configuration_file);
-		if (!f.is_open()) {
-			throw std::runtime_error("Could not open " + configuration_file.string() + " for parsing");
-		}
+	////Extracts the parameter=value from configuration file and saves them to the Configuration object.
+	////uses regex to ensure that any user-formatting errors do not affect the parsing.
+	//mipfinder::Configuration parse(const std::filesystem::path& configuration_file)
+	//{
+	//	LOG(DEBUG) << "Parsing configuration file (" + configuration_file.string() + ")";
+	//	std::ifstream f;
+	//	f.open(configuration_file);
+	//	if (!f.is_open()) {
+	//		throw std::runtime_error("Could not open " + configuration_file.string() + " for parsing");
+	//	}
 
-		std::string line;
-		std::string header;
-		std::unordered_set<std::string> added_params;
-		unsigned int config_line_nr = 0;
-		while (getline(f, line)) {
-			++config_line_nr;
+	//	std::string line;
+	//	std::string header;
+	//	std::unordered_set<std::string> added_params;
+	//	unsigned int config_line_nr = 0;
+	//	mipfinder::Configuration config;
+	//	while (getline(f, line)) {
+	//		++config_line_nr;
 
-			if (line.empty()) {
-				LOG(DEBUG) << "Ignoring empty line";
-				continue;
-			}
+	//		const auto comment_line_token = '#';
+	//		if (line.empty()) {	continue; }
+	//		if (line.front() == comment_line_token) { continue; }
 
-			if (line.front() == '#') {
-				LOG(DEBUG) << "Ignoring comment line";
-				continue;
-			}
+	//		//The logic is as follows:
+	//		//First check for the header line. Due to the way the regexes were written, the parameter=value 
+	//		//regex also matches the header line, therefore we have to check for that first.
+	//		//If there is no match, check whether the line contains the word ":optional:". This signals that
+	//		//the parameter is optional. The flag is set and the line is then matched against the
+	//		//parameter=value regex, extracting the information.
+	//		static const std::regex header_regex(R"((?:^\[)(\S+)(?:\]))");
+	//		static const std::regex param_value_regex(R"((?:^[\s]+)*(?:\:optional\:)?([\S]+)(?:[=])([\S]+)*)");
+	//		std::smatch matches;
 
-			//The logic is as follows:
-			//First check for the header line. Due to the way the regexes were written, the parameter=value 
-			//regex also matches the header line, therefore we have to check for that first.
-			//If there is no match, check whether the line contains the word ":optional:". This signals that
-			//the parameter is optional. The flag is set and the line is then matched against the
-			//parameter=value regex, extracting the information.
-			static const std::regex header_regex(R"((?:^\[)(\S+)(?:\]))");
-			static const std::regex param_value_regex(R"((?:^[\s]+)*(?:\:optional\:)?([\S]+)(?:[=])([\S]+)*)");
-			std::smatch matches;
-			mipfinder::Configuration config;
-			if (std::regex_search(line, matches, header_regex)) {
-				header = matches[1];
-				//If first (or new) header is found, reset the "added_params" set since parameters are allowed
-				//to be duplicated within separate headers.
-				added_params.clear();
-				config[header] = mipfinder::Parameters{};
-				LOG(DEBUG) << "A valid header with value \"" + header << +"\" was found";
-				continue;
-			}
+	//		if (std::regex_search(line, matches, header_regex)) {
+	//			header = matches[1];
+	//			//If a header is found, reset the "added_params" set since parameters are allowed
+	//			//to be duplicated within separate headers.
+	//			added_params.clear();
+	//			config[header] = mipfinder::Parameters{};
+	//			LOG(DEBUG) << "A valid header with value \"" + header << +"\" was found";
+	//			continue;
+	//		}
 
-			bool is_optional = false;
-			if (line.find(":optional:") != std::string::npos) {
-				LOG(DEBUG) << "Optional token was found";
-				is_optional = true;
-			}
+	//		bool is_optional = false;
+	//		if (line.find(":optional:") != std::string::npos) {
+	//			LOG(DEBUG) << "Optional token was found";
+	//			is_optional = true;
+	//		}
 
-			if (std::regex_search(line, matches, param_value_regex)) {
-				std::string parameter = matches[1];
-				std::string value = matches[2];
+	//		if (std::regex_search(line, matches, param_value_regex)) {
+	//			std::string parameter = matches[1];
+	//			std::string value = matches[2];
 
-				if (value.empty() && !is_optional) {
-					//If parameter is not set as optional, value cannot be empty! Leaving it empty is a logic
-					//error.
-					throw std::logic_error(configuration_file.string() + ":" + std::to_string(config_line_nr) + ": Parameter ("
-						+ parameter + ") is not marked as optional and must have a value");
-				}
+	//			if (value.empty() && !is_optional) {
+	//				//If parameter is not set as optional, value cannot be empty! Leaving it empty is a logic
+	//				//error.
+	//				throw std::logic_error(configuration_file.string() + ":" + std::to_string(config_line_nr) + ": Parameter ("
+	//					+ parameter + ") is not marked as optional and must have a value");
+	//			}
 
-				LOG(DEBUG) << "A valid parameter \"" + parameter + "\" with value \"" + value + "\" was found";
-				if (added_params.find(parameter) == added_params.end()) {
-					config[header].insert(parameter, value);
-					added_params.insert(parameter);
-					LOG(DEBUG) << "Added " + parameter + " with " + value + " to " + header;
-					continue;
-				}
-				else {
-					throw std::logic_error(configuration_file.string() + ":" + std::to_string(config_line_nr) + ": Duplicate"
-						" parameters (" + parameter + ") are not allowed");
-				}
-			}
-		}
-		LOG(DEBUG) << "Finised parsing configuration file";
-		return;
-	}
-}
-
-namespace mipfinder
-{
-	void Parameters::insert(const Parameter& parameter, const Value& value)
-	{
-		params_.emplace(parameter, value);
-	}
-
-	const Parameters::Value& Parameters::operator[](const Parameter& parameter) const
-	{
-		return params_.at(parameter);
-	}
-
-	Configuration::Configuration(const std::filesystem::path& configuration_file)
-		: config_file_(configuration_file)
-	{
-		parse(config_file_);
-	}
-
-	const Parameters& mipfinder::Configuration::operator[](const Header& header) const
-	{
-		return config_.contains(header) ? config_.at(header) : Parameters{};
-	}
-}
-
-
-
-
-
-
-std::size_t mipfinder::Configuration::size() const noexcept
-{
-	return config_.size();
-}
-
-std::size_t mipfinder::Parameters::size() const noexcept
-{
-	return params_.size();
+	//			LOG(DEBUG) << "A valid parameter \"" + parameter + "\" with value \"" + value + "\" was found";
+	//			if (added_params.find(parameter) == added_params.end()) {
+	//				config[header].insert(parameter, value);
+	//				added_params.insert(parameter);
+	//				LOG(DEBUG) << "Added " + parameter + " with " + value + " to " + header;
+	//				continue;
+	//			}
+	//			else {
+	//				throw std::logic_error(configuration_file.string() + ":" + std::to_string(config_line_nr) + ": Duplicate"
+	//					" parameters (" + parameter + ") are not allowed");
+	//			}
+	//		}
+	//	}
+	//	LOG(DEBUG) << "Finised parsing configuration file";
+	//	return config;
+	//}
 }
